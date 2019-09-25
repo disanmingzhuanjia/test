11b644418ff6d569fc05862090803ac0ba874451
disanmingzhuanjia <32919347+disanmingzhuanjia@users.noreply.github.com>
2019-09-25 10:49:16 +0800

    Add files via upload

---
+++ proj2/main.c
@@ -0,0 +1,17 @@
+#include "ourhdr.h"
+char buf1[]="abcdefghij";
+char buf2[]="ABCDEFGHIJ";
+int main()
+{
+    int fd;
+	if(fd=creat("file.hole",6)<0);
+	perror("creat error");
+	if(write(fd,buf1,10)!=10)
+	perror("buf1 write error");
+	if(lseek(fd,40,SEEK_SET)==-1)
+	perror("lseek error");
+	if(write(fd,buf2,10)!=10)
+	perror("buf2 write error");
+	system("cat file.hole");
+	return 0;
+}
